9dcc2dc2dbcc6348faa558eb751de4233f932bcd
emilypier <piersoee@miamioh.edu>
2022-04-06 20:07:32 -0400

    file set up for frontend

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import Nav from "../components/Nav";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
+
+const Home = () => {
+  return (
+    <div className="container">
+      {/* <Nav />
+      <CategoryMenu />
+      <ProductList />
+      <Cart /> */}
+      <h1>testing hello hello</h1>
+    </div>
+  );
+};
+
+export default Home;
