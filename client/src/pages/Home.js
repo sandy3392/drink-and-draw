6b50cc10a2e4d52bb9f28250f669450c8fecb9e4
emilypier <piersoee@miamioh.edu>
2022-04-09 10:36:50 -0400

    added css + edited home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Nav from "../components/Nav";
-import CategoryMenu from "../components/CategoryMenu";
+// import Nav from "../components/Nav";
+// import CategoryMenu from "../components/CategoryMenu";
 import ProductList from "../components/ProductList";
 import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
-      <Nav />
-      <CategoryMenu />
+      {/* <Nav /> */}
+      {/* <CategoryMenu /> */}
       <ProductList />
       <Cart />
       <h1>Homepage</h1>
